e24c236deb6037194368fa3b31f552a1bacb4750
Thomas Boerger <tboerger@suse.de>
2014-09-16 14:15:06 +0200

    Raised version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "tboerger@tbpro.de"
 license "Apache 2.0"
 description "Installs/Configures sublime"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version "0.0.1"
+version "0.1.0"
 recipe "sublime", "Installs/Configures sublime"
 
 supports "suse", ">= 12.3"
